6497c8ec3fc8cad9fdcb69d3288882c2790ad165
Leo <leonidas01032002@gmail.com>
2025-11-27 13:59:02 +0100

    initial

---
+++ GameOfLife/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace GameOfLife
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Start-Logik
+        }
+
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Stop-Logik
+        }
+
+        private void RandomButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Random
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Clear
+        }
+
+        private void GameCanvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            // Maus Down
+        }
+
+        private void GameCanvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            // Maus Move
+        }
+
+        private void GameCanvas_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            // Maus Up
+        }
+    }
+}
